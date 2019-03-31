50fe2442ca12a71c5f39e47e036221c82c67a65c
Brandon Stehling <44592622+FlyingMongoose2018@users.noreply.github.com>
2019-03-30 21:57:26 -0500

    Main Activity
    main activity(homescreen) with map activity included

--- app/src/main/java/com/example/aroundtown/MainActivity.java
+++ app/src/main/java/com/example/aroundtown/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.aroundtown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = (ImageButton) findViewById(R.id.ButtonMap);
+        testButton.setOnClickListener(new View.OnClickListener()    {
+            public void onClick(View v) {
+/*
+                Toast.makeText(MainActivity.this, "It Works", Toast.LENGTH_LONG).show();
+*/
+                setContentView(R.layout.activity_maps);
+            }
+        });
+
     }
     //Red Vines, What can't they do?
 }
