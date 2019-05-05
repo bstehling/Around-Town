728aa86f49a0e81f84db4375a42947fd512bd632
Brandon Stehling <bstehling1@gmail.com>
2019-05-05 18:46:53 -0500

    manager activity

--- app/src/main/java/com/example/aroundtown/LoginActivity.java
+++ app/src/main/java/com/example/aroundtown/LoginActivity.java
@@ -343,8 +343,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                if(mEmail.contains("manager")){
+                    Intent intent = new Intent(getApplicationContext(),ManagerMainActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+
+
+               /* Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);*/
 
                 finish();
             } else {
